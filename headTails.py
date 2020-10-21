f2da84131de9eb26c3d3c8f22a974c87c7515c1d
Toussef <youssef@mmneimneh.net>
2020-10-21 13:44:40 +0300

    Create headTails.py

---
+++ headTails.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 13:39:31 2020
+
+@author: Youssef
+"""
+
+import random
+
+fiftyFifty = random.randint(1,2)
+
+if fiftyFifty == 1:
+    print('Heads')
+    
+else:
+    print('Tails')
